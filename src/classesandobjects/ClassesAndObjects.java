6285f07d90cda4bf1035c6dd79b535d53de43286
2022256 <2022256@student.cct.ie>
2023-10-04 12:46:30 +0100

    6 triangles objects

--- src/classesandobjects/ClassesAndObjects.java
+++ src/classesandobjects/ClassesAndObjects.java
@@ -15,6 +15,32 @@ public class ClassesAndObjects {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+       Triangle tri1 = new Triangle();
+       Triangle tri2 = new Triangle();
+       Triangle tri3 = new Triangle();
+       Triangle tri4 = new Triangle();
+       Triangle tri5 = new Triangle();
+       Triangle tri6 = new Triangle();
+       
+       int[] sideLengths = new int[10];
+       
+       sideLengths[0] = 5;
+               sideLengths[1] = 8;
+                       sideLengths[2] = 9;
+                       
+       sideLengths[12] = 4;
+               sideLengths[13] = 7;
+                       sideLengths[14] = 2;
+                       
+       sideLengths[1] = 3;
+       sideLengths[4] = 3;
+       sideLengths[7] = 3;
+       sideLengths[10] = 3;
+       sideLengths[13] = 3;
+       sideLengths[16] = 3;
+                      
+       
     }
     
 }
